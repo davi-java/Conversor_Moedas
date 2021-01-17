ede03d78ec12f0de4d7e97251fa343453bd93915
davi-java <david96711972@gmail.com>
2021-01-17 08:49:29 -0400

    style class Conversor

--- src/Conversor/index.js
+++ src/Conversor/index.js
@@ -21,6 +21,7 @@ export default class Conversor extends Component {
                     onChangeText={() => { }}
                     style={styles.input}
                     keyboardType="numeric"
+                    underlineColorAndroid="transparent"
                 />
                 
                 <TouchableOpacity style={styles.btn}>
@@ -39,6 +40,44 @@ export default class Conversor extends Component {
 
 const styles = StyleSheet.create({
     container: {
-
+        flex:1,
+        alignItems:'center',
+        justifyContent:'center'
+    },
+    title:{
+        fontSize:30,
+        fontWeight:'bold',
+        color:'#000'
+    },
+    input:{
+        height: 45,
+        width: 300,
+        backgroundColor:'#CCC',
+        textAlign:'center',
+        fontSize: 20,
+        borderRadius:10,
+        color:'#000',
+        marginTop: 16
+    },
+    btn:{
+        backgroundColor:'#F00',
+        height:45,
+        width: 150,
+        marginTop: 15,
+        borderRadius: 10,
+        alignItems:'center',
+        justifyContent:'center'
+    },
+    textBtn:{
+        fontSize: 20,
+        color:'#fff',
+        fontWeight:'bold',
+        fontStyle:'italic'
+    },
+    valueConverted:{
+        fontSize: 30,
+        fontWeight:'bold',
+        color:'#000000',
+        marginTop: 15
     }
 })
